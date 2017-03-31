83767abb3dfc85c4c083cfa707cedee2383872d6
Andrew Thompson <andrewthompson@Andrews-MacBook-Pro.local>
2017-03-31 16:56:59 -0400

    send and recieve  script

---
+++ recieve_log_script.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         type='fanout')
+
+result = channel.queue_declare(exclusive=True)
+all_flex = result.method.queue
+
+channel.queue_bind(exchange='logs',
+                   queue=all_flex)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+
+channel.basic_consume(callback,
+                      queue=all_flex,
+                      no_ack=True)
+
+channel.start_consuming()
